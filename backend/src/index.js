7ad71e31a59c91c59b0884a98474c901fa0ae9da
Aashiq-Edavalapati <aashiqedavalapati58@gmail.com>
2025-02-22 17:44:18 +0530

    Switched from local db to neon db

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import router from './routes/api.route.js';
 import cors from 'cors';
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 const app = express();
 
